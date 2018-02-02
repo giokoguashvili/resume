b0895f7f7e7dd9cad2196d3c50a6c161e9f40df3
Giorgi Koguashvili <kogo27@gmail.com>
2018-02-02 13:29:25 +0400

    Update makepdf.ps1

--- makepdf.ps1
+++ makepdf.ps1
@@ -1,1 +1,26 @@
-echo "hello world"
+$file = "README"
+$fileName = "$file.md"
+$uri = "http://www.markdowntopdf.com/app/upload"
+$outputFileName = "Giorgi Koguashvili CV.pdf"
+$currentPath = Convert-Path .
+$filePath="$currentPath\$fileName"
+
+$fileBin = [System.IO.File]::ReadAlltext($filePath)
+$boundary = [System.Guid]::NewGuid().ToString()
+$LF = "`r`n"
+$bodyLines = (
+    "--$boundary",
+    "Content-Disposition: form-data; name=`"file`"; filename=`"$fileName`"",
+    "Content-Type: application/octet-stream$LF",
+    $fileBin,
+    "--$boundary--$LF"
+) -join $LF
+
+$response = Invoke-RestMethod -Uri $uri -Method Post -ContentType "multipart/form-data; boundary=`"$boundary`"" -Body $bodyLines
+echo $response
+
+Start-Sleep -s 1
+
+$url = "http://www.markdowntopdf.com/app/download/$($response.foldername)/$file"
+echo $url
+Invoke-WebRequest -Uri $url -OutFile $outputFileName
